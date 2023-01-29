9ea176d28a361fa49629a73c5a513f370bc46846
David Restivo <davidrestivo27@gmail.com>
2023-01-28 20:07:25 -0500

    worked on the models

--- models/index.js
+++ models/index.js
@@ -6,11 +6,40 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsToMany(Category, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  as: 'Product'
+});
+
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through: {
+    model: Category,
+    unique: false
+  },
+  as: 'Category'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  through: {
+    model: Product,
+    unique: false
+  },
+  as: 'ProductTags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'Tags'
+});
 
 module.exports = {
   Product,
